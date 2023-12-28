4f3677e6443f477a7da3b3f8e5dac94c39522977
DiogoS77 <diogo.messi.18@hotmail.com>
2023-12-28 22:11:47 +0000

    added new fonts, and started page , by adding simple text and portrait

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,21 +1,21 @@
-import React from 'react';
+// Home.jsx
+import React from "react";
+import "../../css/Home.css";
+import Portrait from "../../images/portrait.png";
 
 export default function Home() {
   return (
-    <div>
-      <h1>Home Page</h1>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed neque
-        velit, lobortis ut magna varius, blandit rhoncus sem. Morbi lacinia nisi
-        ac dui fermentum, sed luctus urna tincidunt. Etiam ut feugiat ex. Cras
-        non risus mi. Curabitur mattis rutrum ipsum, ut aliquet urna imperdiet
-        ac. Sed nec nulla aliquam, bibendum odio eget, vestibulum tortor. Cras
-        rutrum ligula in tincidunt commodo. Morbi sit amet mollis orci, in
-        tristique ex. Donec nec ornare elit. Donec blandit est sed risus feugiat
-        porttitor. Vestibulum molestie hendrerit massa non consequat. Vestibulum
-        vitae lorem tortor. In elementum ultricies tempus. Interdum et malesuada
-        fames ac ante ipsum primis in faucibus.
-      </p>
+    <div className="home-container">
+      <div className="content">
+        <div className="text-content">
+          <h1 className="header">Front-End Web Developer</h1>
+          <p className="intro-text">
+            Hi, I am Diogo Sousa, a passionate front-end web developer based in
+            Oldbury, England.
+          </p>
+        </div>
+        <img className="profile-image" src={Portrait} alt="Profile" />
+      </div>
     </div>
   );
 }
