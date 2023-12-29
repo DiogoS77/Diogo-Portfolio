ba5a3b86b45e9a6f2ae0555dc45a851b2ebe5aae
DiogoS77 <diogo.messi.18@hotmail.com>
2023-12-29 22:10:32 +0000

    added 2 images to the introduction and adjusted sizes

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -2,16 +2,30 @@
 import React from "react";
 import "../../css/Home.css";
 import Portrait from "../../images/portrait.png";
+import BlueLocationPin from "../../images/blueLocationPin.png";
+import WavingBlueHand from "../../images/wavingBlueEmoji.png";
 
 export default function Home() {
   return (
     <div className="home-container">
       <div className="content">
         <div className="text-content">
-          <h1 className="header">Front-End Web Developer</h1>
+          <h1 className="header">
+            Front-End Web Developer{" "}
+            <img
+              className="waving-hand"
+              src={WavingBlueHand}
+              alt="Waving Hand"
+            />
+          </h1>
           <p className="intro-text">
             Hi, I am Diogo Sousa, a passionate front-end web developer based in
-            Oldbury, England.
+            Oldbury, England.{" "}
+            <img
+              src={BlueLocationPin}
+              alt="Location Pin"
+              className="location-pin"
+            />
           </p>
         </div>
         <img className="profile-image" src={Portrait} alt="Profile" />
